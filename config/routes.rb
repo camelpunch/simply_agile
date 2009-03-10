89b7f9e17c2846b81008080a0dec064146b0d611
John Cinnamond <jc@jandaweb.com>
2009-03-10 17:10:49 +0000

    Changed Projects#show to replace a list of stories with links to different types.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :session
   map.resources :projects do |project|
     project.resources :iterations
-    project.resources :stories do |story|
+    project.resources(:stories, :collection => { :finished => :get }) do |story|
       story.resources :acceptance_criteria
     end
   end
