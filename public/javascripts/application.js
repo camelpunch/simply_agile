815d2379037a8e95f4fca9abbc3d794bda8f69da
Andrew Bruce <ab@jandaweb.com>
2009-03-12 18:56:24 +0000

    dragging / dropping in iterations working. needs to be nice looking / acting

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -17,6 +17,44 @@ $(document).ready(function() {
     // start toggles
     StoryToggler.init();
   }
+
+  // iterations/show when active
+  if ($('body#iterations_show .section form.edit_story')[0]) {
+
+    // make droppables for each input box
+    $('input[name="story[status]"]').each( function() {
+      $(this).parents('form')
+        .append('<div id="droppable_' + this.id + '">' + this.id + '</div>');
+
+      var value = $(this).val();
+
+      var droppable = $('#droppable_' + this.id)
+        .droppable({ 
+          drop: function(ev, ui) { 
+            var id_parts = this.id.split('_');
+            var story_id = id_parts[id_parts.length - 1];
+            $('li#story_'+story_id+' ol input').val([value]); // checks the radio button
+            $('.ui-droppable').removeClass('ui-state-highlight');
+            $(this).addClass('ui-state-highlight');
+          }
+        });
+
+      // initialisation
+      if ($(this).attr('checked')) {
+        // make a draggable
+        droppable_position = droppable.position();
+        droppable.addClass('ui-state-highlight');
+
+        $('form').append('<div id="draggable_' + this.id + '"></div>');
+
+        $('#draggable_' + this.id)
+          .draggable()
+          .css('position', 'absolute')
+          .css('top', droppable_position.top)
+          .css('left', droppable_position.left);
+      }
+    });
+  }
 });
 
 // add header to AJAX requests to play nice with Rails' content negotiation
