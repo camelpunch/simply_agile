e53c45ae7e9c960a605818767946acc370b573dc
Andrew Bruce <ab@jandaweb.com>
2009-03-12 13:03:28 +0000

    layout with graphics new breadcrumbs style

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
   map.resource :session
+  map.resources :iterations
+  map.resources :organisations
   map.resources :projects do |project|
     project.resources :iterations
     project.resources :stories do |story|
       story.resources :acceptance_criteria
     end
   end
+  map.resources :users
   map.root :controller => 'home', :action => 'show'
 end
