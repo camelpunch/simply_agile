791d7fa9c9739132194b93ec21ab00cd5ded45aa
Andrew Bruce <ab@jandaweb.com>
2009-03-24 16:46:34 +0000

    better less / more toggling added header div element to _story.html.erb

--- public/javascripts/story.js
+++ public/javascripts/story.js
@@ -1,30 +1,78 @@
 function Story(element) {
   var instance = this;
   this.element = $(element);
+  this.story_content = this.element.find('ol');
   this.acceptance_criteria = this.element.find('.acceptance_criteria');
 
-  // add 'more' link if needed
-  if (this.element.find('.acceptance_criteria')[0]) {
-    this.element.find('ol li:last-child')
-      .css({position: 'relative'})
-      
-      .append('<a class="more" href="#more">More</a>')
+  this.createContainer();
+  this.createLessAnchor();
 
-      .find('a.more').click(function() {
-        var acceptance_criteria = $(this).parents('.story').find('.acceptance_criteria');
-        acceptance_criteria.toggle();    
-
-        instance.setMoreHtml();
-
-        return false;
-      });
-
-    this.setMoreHtml();
+  if (this.acceptance_criteria[0]) {
+    this.createMoreAnchor();
   }
 }
 Story.prototype = {
+  createContainer: function() {
+    this.element.find('h3').after('<div class="less_more"></div>');
+    this.container = this.element.find('.less_more');
+  },
+
+  createMoreAnchor: function() {
+    var instance = this;
+
+    // add 'more' link if needed
+    this.container
+      .append('<a class="more" href="#more">More</a>')
+
+      .find('a.more').click(function() {
+        instance.acceptance_criteria.toggle();    
+        instance.setMoreHtml();
+        return false;
+      });
+
+    this.more = this.container.find('a.more');
+    this.setMoreHtml();
+  },
+
   setMoreHtml: function() {
-    this.element.find('a.more')
-      .html(this.acceptance_criteria.is(':visible') ? '« Less' : 'More »')
+    var html;
+
+    if (this.acceptance_criteria.is(':visible')) { 
+      html = '« Less'
+      this.less.hide();
+    } else {
+      html = 'More »';
+      this.less.show();
+    }
+    this.more.html(html);
+  },
+
+  createLessAnchor: function() {
+    var instance = this;
+
+    this.container.append('<a class="less" href="#less">Less</a>');
+    this.less = this.container.find('a.less');
+
+    this.less.click(function() {
+      instance.story_content.toggle();
+      instance.setLessHtml();
+      return false;
+    });
+
+    this.setLessHtml();
+  },
+
+  setLessHtml: function() {
+    var html;
+
+    if (this.story_content.is(':visible')) {
+      html = '« Less';
+      if (this.more) this.more.show();
+    } else {
+      html = 'More »';
+      if (this.more) this.more.hide();
+    }
+
+    this.less.html(html);
   }
 }
