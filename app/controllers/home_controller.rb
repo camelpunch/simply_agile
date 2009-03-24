9974696946c1a69593c9fcfd211cdcd96d600375
John Cinnamond <jc@jandaweb.com>
2009-03-24 18:46:46 +0000

    Fixing home and projects pages to cope with multiple organisations.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
-  before_filter :current_organisation
   before_filter :get_active_iterations
 
   def show
@@ -8,6 +7,6 @@ class HomeController < ApplicationController
   protected
 
   def get_active_iterations
-    @active_iterations = @organisation.iterations.active
+    @active_iterations = current_organisation.iterations.active
   end
 end
