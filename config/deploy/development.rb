1b6661149e11791587d2b66942b881c0b4a344ee
Andrew Bruce <ab@jandaweb.com>
2009-03-27 11:08:05 +0000

    set rails_env in config/deploy/development.rb

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,5 +1,6 @@
 set :deploy_to, "/rails/simply-agile.dev.jandaweb.com"
 
+set :rails_env, 'development'
 set :domain, "dev.jandaweb.com"
 server domain, :app, :web
 role :db, domain, :primary => true
