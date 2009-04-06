b89809382e69c2312f5b05654e8b2565144379e5
Andrew Bruce <ab@jandaweb.com>
2009-04-06 15:19:21 +0100

    oops, forgot to commit this payment methods index view spec fix

--- spec/views/payment_methods/index.html.erb_spec.rb
+++ spec/views/payment_methods/index.html.erb_spec.rb
@@ -2,8 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/payment_methods/index" do
   before(:each) do
+    organisation = Organisations.create_organisation!
+    organisation.next_payment_date = Date.today
     payment_method = 
-      PaymentMethod.new(:organisation => Organisations.create_organisation!,
+      PaymentMethod.new(:organisation => organisation,
                         :year => 2.years.from_now.year,
                         :month => 1)
     payment_method.build_billing_address
