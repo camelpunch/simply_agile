e6207361d4932d9fb7518b9a2607334ddfc3b0a7
Andrew Bruce <ab@jandaweb.com>
2009-06-04 00:49:32 +0100

    cope with user not being logged in on keyboard shortcuts (no CSS, but unlikely that user will visit without log in)

--- app/controllers/keyboard_shortcuts_controller.rb
+++ app/controllers/keyboard_shortcuts_controller.rb
@@ -3,6 +3,9 @@ class KeyboardShortcutsController < ApplicationController
   skip_before_filter :select_organisation
 
   def index
+    unless session[:user_id]
+      render :layout => 'landing'
+    end
   end
 
 end
